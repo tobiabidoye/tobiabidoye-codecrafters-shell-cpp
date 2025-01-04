ae35d6f5d0c1fb9c58e06a2683918aa00ab1cdfa
Tobi Abidoye <tobiabidoye14@gmail.com>
2025-01-04 01:59:16 -0500

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,36 @@ int main() {
         
       std::cout << "$ ";
       std::string input;
+      std::string targetEcho = "echo";
+      bool echoTrue = true;
       std::getline(std::cin, input);
+          
+      for(int i = 0; i < 4; i++){
+        if(input[i] != targetEcho[i]){
+            echoTrue = false;
+        }
+      }
       
+
       if(input == "exit 0"){
         isValid = true;
-
+        break;
+      }else if(echoTrue){
+        for(int i = 5; i < input.size(); i++){
+            std::cout << input[i];
+        }
+        std::cout << std::endl;
       }else{
 
+
         std::cout << input << ": command not found" << std::endl;
+
       }
+
+
+      
+
+      
        
 
   }
