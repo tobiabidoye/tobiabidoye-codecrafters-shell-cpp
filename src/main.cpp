2c7a4b3a7a54b091e01fe3993e633db18099adaa
Tobi Abidoye <tobiabidoye14@gmail.com>
2025-01-04 01:32:25 -0500

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,15 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
+  bool isValid = false;
+  while(!isValid){
 
-  std::string input;
-  std::getline(std::cin, input);
+      std::cout << "$ ";
+      std::string input;
+      std::getline(std::cin, input);
 
-  std::cout << input << ": command not found" << std::endl;
+      std::cout << input << ": command not found" << std::endl;
+
+
+  }
 }
